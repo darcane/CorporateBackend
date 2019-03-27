c1af27c51a1fb272b1ef74921fd3d163f17be99a
Erdem Darcan <erdemdarcan@hotmail.com.tr>
2019-03-28 02:41:52 +0300

    UnitTest for EntityFramwork
    Connection string added to DataAccess.Tests section without compromising the db

---
+++ Corp.AdventureWorks.DataAccess.Tests/EntityFrameworkTests/EntityFrameworkTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Corp.AdventureWorks.DataAccess.Concrete.EntityFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Corp.Northwind.DataAccess.Tests.EntityFrameworkTests
+{
+    [TestClass]
+    public class EntityFrameworkTest
+    {
+        [TestMethod]
+        public void Get_all_returns_more_than_zero_elements()
+        {
+            EfProductDal productDal = new EfProductDal();
+            var result = productDal.GetList();
+
+            Assert.AreNotEqual(0,result.Count);
+        }
+    }
+}
