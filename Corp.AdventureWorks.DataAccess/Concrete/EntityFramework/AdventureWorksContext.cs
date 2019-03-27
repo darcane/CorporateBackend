4595fe5f4fe579c7252e66708498c47b8617edba
Erdem Darcan <erdemdarcan@hotmail.com.tr>
2019-03-28 02:22:08 +0300

    EntityFramework mapping

--- Corp.AdventureWorks.DataAccess/Concrete/EntityFramework/AdventureWorksContext.cs
+++ Corp.AdventureWorks.DataAccess/Concrete/EntityFramework/AdventureWorksContext.cs
@@ -1,9 +1,20 @@
 ﻿using System.Data.Entity;
+using Corp.AdventureWorks.DataAccess.Concrete.EntityFramework.Mappings;
+using Corp.AdventureWorks.Entities.Concrete;
 
 namespace Corp.AdventureWorks.DataAccess.Concrete.EntityFramework
 {
     public class AdventureWorksContext : DbContext
     {
-        
+        public AdventureWorksContext()
+        {
+            Database.SetInitializer<AdventureWorksContext>(null); // block code from generating tables on db
+        }
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ProductMap());
+        }
     }
 }
---
+++ Corp.AdventureWorks.DataAccess/Concrete/EntityFramework/Mappings/ProductMap.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Corp.AdventureWorks.Entities.Concrete;
+
+namespace Corp.AdventureWorks.DataAccess.Concrete.EntityFramework.Mappings
+{
+    public class ProductMap : EntityTypeConfiguration<Product>
+    {
+        public ProductMap()
+        {
+            ToTable(@"Product", @"SalesLT");
+            HasKey(x => x.ProductId);
+
+            Property(x => x.ProductId).HasColumnName(@"ProductId");
+            Property(x => x.Name).HasColumnName(@"Name");
+            Property(x => x.ProductCategoryId).HasColumnName(@"ProductCategoryId");
+            Property(x => x.ProductNumber).HasColumnName(@"ProductNumber");
+            Property(x => x.ListPrice).HasColumnName(@"ListPrice");
+        }
+    }
+}
