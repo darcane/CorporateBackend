55a6d3fcfdf1d95fcb7abfc47c16c641c3761831
Erdem Darcan <darcane@itu.edu.tr>
2019-03-29 15:56:14 +0300

    Business layer Managers added

---
+++ Corp.AdventureWorks.Business/Abstract/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Corp.AdventureWorks.Entities.Concrete;
+
+namespace Corp.AdventureWorks.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+        Product Add(Product product);
+
+    }
+}
---
+++ Corp.AdventureWorks.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Corp.AdventureWorks.Business.Abstract;
+using Corp.AdventureWorks.DataAccess.Abstract;
+using Corp.AdventureWorks.Entities.Concrete;
+
+namespace Corp.AdventureWorks.Business.Concrete.Managers
+{
+    public class ProductManager : IProductService
+    {
+        private readonly IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+    }
+}
