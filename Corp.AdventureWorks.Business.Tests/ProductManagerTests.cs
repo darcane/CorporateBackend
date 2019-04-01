7eb7e8b45196d6687d4cfd00360250e02c8180c2
Erdem Darcan <erdemdarcan@hotmail.com.tr>
2019-04-01 23:07:12 +0300

    Aspect UnitTest

---
+++ Corp.AdventureWorks.Business.Tests/ProductManagerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Corp.AdventureWorks.Business.Concrete.Managers;
+using Corp.AdventureWorks.DataAccess.Abstract;
+using Corp.AdventureWorks.Entities.Concrete;
+using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Corp.AdventureWorks.Business.Tests
+{
+    [TestClass]
+    public class ProductManagerTests
+    {
+        [ExpectedException(typeof(ValidationException))]
+        [TestMethod]
+        public void Product_validation_check_when_a_default_product_tried_to_be_added()
+        {
+            Mock<IProductDal> mock = new Mock<IProductDal>();
+            ProductManager productManager = new ProductManager(mock.Object);
+
+            productManager.Add(new Product());
+        }
+    }
+}
