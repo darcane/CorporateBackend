513b28151a9028fcda5c3d1f204f49c3003940c0
Erdem Darcan <erdemdarcan@hotmail.com.tr>
2019-04-01 22:28:54 +0300

    FluentValidation referenced

---
+++ Corp.AdventureWorks.Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,17 @@
+﻿using Corp.AdventureWorks.Entities.Concrete;
+using FluentValidation;
+
+namespace Corp.AdventureWorks.Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductCategoryId).NotEmpty().WithMessage("Category Id can not be empty");
+            RuleFor(p => p.Name).NotEmpty();
+            RuleFor(p => p.ListPrice).GreaterThan(0);
+            RuleFor(p => p.ListPrice).GreaterThan(20).When(p=>p.ProductCategoryId == 3);
+            RuleFor(p => p.ProductNumber).Length(4, 10);
+        }
+    }
+}
